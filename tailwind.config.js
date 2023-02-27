45db8b6da2aa8b27022a43b119dfac1bf9706ea5
Bruno <bruno.gianini@readmore.com.br>
2023-02-26 22:22:30 -0300

    custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,17 @@ module.exports = {
   ],
   theme: {
     extend: {},
+
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'primary': '#30343F',
+      'secondary': '#FAFAFF',
+      'pink-white': '#E4D9FF',
+      'solid-blue': '#273469',
+      'dark-blue': '#1E2749',
+    },
+
   },
   plugins: [],
 }
