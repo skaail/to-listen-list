23811c8711a3b622018e6e9f0408f6e4ce5b93fc
Bruno <bruno.gianini@readmore.com.br>
2023-02-26 22:50:39 -0300

    Pages routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Sidebar from './components/Sidebar';
+
 import Home from './pages/Home';
+import Profile from './pages/Profile';
+import List from './pages/List';
+import Review from './pages/Review';
+import Ranking from './pages/Ranking';
 
 function App() {
   return (
@@ -13,6 +18,10 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/list' element={<List />} />
+        <Route path='/review' element={<Review />} />
+        <Route path='/ranking' element={<Ranking />} />
       </Routes>
     </BrowserRouter>
     </>
